a5b7e9b5724179e20bb67a1b3f2251fb52ba5326
王良 <wangliang01@corp.netease.com>
2020-07-04 13:10:19 +0800

    null

--- src/leetCode/slide_window/slide_window_76.java
+++ src/leetCode/slide_window/slide_window_76.java
@@ -9,8 +9,25 @@ import java.util.Queue;
 
 /**
  * Created by wangliang01 on 2020/7/2 using IDEA.
+ *
+ * 76. 最小覆盖子串
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ *
+ * 示例：
+ *
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ * 说明：
+ *
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-window-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class window_76 {
+public class slide_window_76 {
+    // todo 这个代码是自己想的，比较便利，但不能完全成功
     public String minWindow(String s, String t) {
         if(s == null || t == null || s.length() < t.length()) {
             return "";
